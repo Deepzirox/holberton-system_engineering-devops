c3b87e8031988bbe03a861c01851f2ada12f59ad
Camilo tobon <1611@holbertonschool.com>
2020-06-22 20:27:09 -0500

    adding

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -17,15 +17,15 @@ void starway(void)
  */
 int main(void)
 {
-	pid_t pid;
-	int x;
-
-	for (x = 0; x < 5; x++)
+	int i;
+	pid_t child;
+	
+	for (i = 0; i < 5; i++)
 	{
-		pid = fork();
-		if (pid < 1)
+		child = fork();
+		if (child < 1)
 			return (0);
-		printf("Zombie process created, PID: %d\n", pid);
+		printf("Zombie process created, PID: %d\n", child);
 	}
 	starway();
 	return (0);
