81182618f815fce46a37909d687732e6af582c9f
Camilo Tobon <1611@holbertonschool.com>
2020-08-03 22:24:11 -0500

    wtf

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # puppet create file
 file { '/tmp/holberton':
       ensure  => present,
-      content => 'I Love Puppet',
+      content => 'I love Puppet',
       owner   => 'www-data',
       group   => 'www-data',
-      mode    => 0744,
+      mode    => '0744',
     }
