280cb977d7d5f34c79304b49c467578c6b2e2727
Camilo Tobon <1611@holbertonschool.com>
2020-08-04 19:47:53 -0500

    add puppet

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+ # Configure ssh config
+ 
+ file_line{'Turn off passwd auth':
+ ensure => 'present',
+ path   => '/etc/ssh/ssh_config',
+ line   => 'PasswordAuthentication no',
+ match  => '^PasswordAuthentication',
+ }
+ 
+ file_line{'Declare identity file':
+ ensure => 'present',
+ path   => '/etc/ssh/ssh_config',
+ line   => 'IdentityFile ~/.ssh/holberton',
+ match  => '^IdentityFile',
+ }
+ 
