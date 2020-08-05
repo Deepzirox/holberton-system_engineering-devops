5cbe51229ad6c691a843875b557165c345885f50
Camilo Tobon <1611@holbertonschool.com>
2020-08-04 19:50:35 -0500

    add

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,16 +1,13 @@
  # Configure ssh config
  
- file_line{'Turn off passwd auth':
- ensure => 'present',
- path   => '/etc/ssh/ssh_config',
- line   => 'PasswordAuthentication no',
- match  => '^PasswordAuthentication',
- }
+file_line {'Declare identity file':
+  replace => true,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/holberton'
+}
  
- file_line{'Declare identity file':
- ensure => 'present',
- path   => '/etc/ssh/ssh_config',
- line   => 'IdentityFile ~/.ssh/holberton',
- match  => '^IdentityFile',
- }
- 
+file_line {'Turn off passwd auth':
+  replace => true,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no'
+}
