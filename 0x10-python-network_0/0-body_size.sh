74dc10b4150923edec20b172e7a307d9d19e3124
Your Name <you@example.com>
2020-08-18 22:24:23 +0000

    add fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
 # GET THE BYTES SIZE OF A RESPONSE
-curl $1 -s -w '%{size_download}' -o tmpfile
-BYTES=$(cat tmpfile | tr -d -c 0-9)
-echo "$BYTES"
-rm -rf tmpfile
+curl "$1" -s -w '%{size_download}' | tr -d -c 0-9 && echo ""
