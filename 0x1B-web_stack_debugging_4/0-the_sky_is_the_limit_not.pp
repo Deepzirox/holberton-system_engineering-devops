c747ab36de43f326317eaf060ec23ccd639ad1e8
Andres Camilo <1611@holbertonschool.com>
2020-10-08 23:26:55 -0500

    add

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,7 +5,8 @@ exec { 'change ulimit':
     path    => '/bin',
     command => "sed -i 's/15/2000/g' /etc/default/nginx"
 }
- 
+
+
 exec { 'nginx restart':
     path    => '/etc/init.d',
     command => 'nginx restart'
